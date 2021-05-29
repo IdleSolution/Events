64956a459fdd4104c9d14c8337b94c18c17efc13
IdleSolution <idlesoluszyn@gmail.com>
2021-05-29 13:11:16 +0200

    Atendee and Activity domains added

---
+++ Domain/Atendee.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+
+namespace Domain
+{
+    public class Atendee
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public ICollection<Activity> Activities { get; set; }
+    }
+}
