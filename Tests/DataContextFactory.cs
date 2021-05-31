a150e1b462070ef073f35e45d6cdbafab84d6ff7
IdleSolution <idlesoluszyn@gmail.com>
2021-05-31 17:30:39 +0200

    Added seeding sample data for testing

--- Tests/DataContextFactory.cs
+++ Tests/DataContextFactory.cs
@@ -1,3 +1,4 @@
+using Domain;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 using Persistence;
@@ -18,6 +19,36 @@ namespace Tests
             dbContext.Database.EnsureCreated();
 
             return dbContext;
+        }
+
+        public static void SeedSampleData(DataContext dbContext)
+        {
+            var id = new Guid();
+            var activity = new Activity
+            {
+                Id = id,
+                Title = "Event1",
+                Description = "Nice event",
+                Category = "Test",
+                Date = DateTime.Now,
+                Venue = "Park",
+                City = "Krakow"
+            };
+            var atendee = new Atendee
+            {
+                Email = "test@gmail.com",
+                Name = "Tester"
+            };
+
+            dbContext.Activities.Add(activity);
+
+            dbContext.Atendees.Add(atendee);
+
+            dbContext.ActivityAtendees.Add(new ActivityAtendee
+            {
+                Atendee = atendee,
+                Activity = activity
+            });
         }
     }
 }
